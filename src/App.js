81edfb666386e8d8d7961f68fb972d42089ac6db
silviustoian <silviu.stoian96@gmail.com>
2020-11-04 20:34:44 +0200

    Chuck Norris Joke Generator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+const API_URL = `http://api.icndb.com/jokes/random`;
+
 function App() {
+  const [joke, setJoke] = useState('');
+
+  const generateJoke = () => {
+    fetch(API_URL)
+      .then(res => res.json())
+      .then(data => setJoke(data.value.joke))
+  }
+
+  useEffect(() => {
+    generateJoke();
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="box">
+      <h2>Chuck Norris Jokes Generator</h2>
+      <p>{joke}</p>
+      <button onClick={generateJoke}>Get a joke <span role="img">😄</span>  </button>
     </div>
   );
 }
